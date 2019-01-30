7364bfc1f0c50b3d94ab2a72208439fae3ef0003
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-01-30 08:01:11 +0100

    Only expose `inngangsvilkår` to clients, the rest are internal

--- src/main/kotlin/no/nav/helse/Inngangsvilkår.kt
+++ src/main/kotlin/no/nav/helse/Inngangsvilkår.kt
@@ -7,42 +7,42 @@ import no.nav.nare.core.specifications.Spesifikasjon
 import no.nav.nare.core.specifications.ikke
 import java.time.LocalDate
 
-private val kanskje = Spesifikasjon<Søknad>(
+internal val kanskje = Spesifikasjon<Søknad>(
    beskrivelse = "Vi har ikke nok informasjon til å kunne gi et entydig svar.",
    identitet = "Ufullstendig informasjonsgrunnlag") { søknad -> Evaluering.kanskje("Vi har ikke nok informasjon til å kunne gi et entydig svar.") }
 
-val harVærtIArbeidIMinstFireUker = Spesifikasjon<Søknad>(
+internal val harVærtIArbeidIMinstFireUker = Spesifikasjon<Søknad>(
    beskrivelse = "Har søker vært i arbeid i minst fire uker?",
    identitet = "§ 8-2 første ledd") { søknad -> søkerHarVærtIArbeid(søknad.førsteSykdomsdag, søknad.datoForAnsettelse) }
 
-val harOppfyltOpptjeningstid = (harVærtIArbeidIMinstFireUker eller kanskje).med(
+internal val harOppfyltOpptjeningstid = (harVærtIArbeidIMinstFireUker eller kanskje).med(
    beskrivelse = "Oppfyller søker krav om opptjeningstid?",
    identitet = "§ 8-2"
 )
 
-val boddeINorgeISykdomsperioden = Spesifikasjon<Søknad>(
+internal val boddeINorgeISykdomsperioden = Spesifikasjon<Søknad>(
    beskrivelse = "Bodde søker i Norge da han eller hun ble syk?",
    identitet = "§ 2-1 første ledd") { søknad -> søkerBorINorge(søknad.bostedlandISykdomsperiode) }
 
-val harOppfyltMedlemskap = (boddeINorgeISykdomsperioden eller kanskje).med(
+internal val harOppfyltMedlemskap = (boddeINorgeISykdomsperioden eller kanskje).med(
    beskrivelse = "Oppfyller søker krav om medlemskap?",
    identitet = "Kapittel 2. Medlemskap"
 )
 
-val harAndreYtelser = Spesifikasjon<Søknad>(
+internal val harAndreYtelser = Spesifikasjon<Søknad>(
    beskrivelse = "Har søker andre ytelser?",
    identitet = "") { søknad -> søkerHarAndreYtelser(søknad.ytelser) }
 
-val harIngenYtelserSomIkkeKanKombineresMedSykepenger = ikke(harAndreYtelser).eller(kanskje).med(
+internal val harIngenYtelserSomIkkeKanKombineresMedSykepenger = ikke(harAndreYtelser).eller(kanskje).med(
    beskrivelse = "Har søker andre ytelser som ikke kan kombineres med sykepenger?",
    identitet = "Forenkling av andre ytelser"
 )
 
-val erSendtInnenTreMåneder = Spesifikasjon<Søknad>(
+internal val erSendtInnenTreMåneder = Spesifikasjon<Søknad>(
    beskrivelse = "Er søknad sendt innen 3 måneder etter måneden for første dag i søknadsperioden",
    identitet = "§ 22-13 tredje ledd") { søknad -> søkerHarSendtSøknadInnenTreMåneder(søknad.søknadSendt, søknad.førsteDagSøknadGjelderFor) }
 
-val erKravetFremsattInnenFrist = (erSendtInnenTreMåneder eller kanskje).med(
+internal val erKravetFremsattInnenFrist = (erSendtInnenTreMåneder eller kanskje).med(
    beskrivelse = "Er kravet fremsatt innen frist?",
    identitet = "§ 22-13"
 )
