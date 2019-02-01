eca9941be2953741273241a1b9283a069b98961c
royag-nav <roy.andre.gundersen@nav.no>
2019-02-01 07:32:26 +0100

    sammenlikn resultat, ja

--- src/test/kotlin/no/nav/helse/Assertions.kt
+++ src/test/kotlin/no/nav/helse/Assertions.kt
@@ -4,6 +4,6 @@ import no.nav.nare.core.evaluations.Evaluering
 import no.nav.nare.core.evaluations.Resultat
 import org.junit.jupiter.api.Assertions.assertEquals
 
-fun assertJa(evaluering: Evaluering) = assertEquals(evaluering, Resultat.JA)
-fun assertNei(evaluering: Evaluering) = assertEquals(evaluering, Resultat.NEI)
-fun assertKanskje(evaluering: Evaluering) = assertEquals(evaluering, Resultat.KANSKJE)
+fun assertJa(evaluering: Evaluering) = assertEquals(evaluering.resultat, Resultat.JA)
+fun assertNei(evaluering: Evaluering) = assertEquals(evaluering.resultat, Resultat.NEI)
+fun assertKanskje(evaluering: Evaluering) = assertEquals(evaluering.resultat, Resultat.KANSKJE)
