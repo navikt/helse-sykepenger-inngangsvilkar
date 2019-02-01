7d9ac2a126722afb41b8faaa9a6f05e880e4b834
royag-nav <roy.andre.gundersen@nav.no>
2019-02-01 12:57:28 +0100

    kopierte inn NARE (inntil videre, til finner ut hvordan laste inn som bibliotek)

---
+++ src/main/kotlin/no/nav/nare/core/specifications/Spesifikasjon.kt
@@ -0,0 +1,53 @@
+package no.nav.nare.core.specifications
+
+import no.nav.nare.core.evaluations.Evaluering
+import no.nav.nare.core.evaluations.Evaluering.Companion.evaluer
+
+
+data class Spesifikasjon<T>(
+   val beskrivelse: String,
+   val identitet: String,
+   val children: List<Spesifikasjon<T>> = emptyList(),
+   val implementasjon: (t: T) -> Evaluering) {
+
+   fun evaluer(t: T): Evaluering {
+      return evaluer(
+         beskrivelse = beskrivelse,
+         identitet = identitet,
+         eval = implementasjon.invoke(t))
+   }
+
+   infix fun og(other: Spesifikasjon<T>): Spesifikasjon<T> {
+      return Spesifikasjon(
+         beskrivelse = "$beskrivelse OG ${other.beskrivelse}",
+         identitet = "$identitet OG ${other.identitet}",
+         children = listOf(this, other),
+         implementasjon = { t -> evaluer(t) og other.evaluer(t) }
+      )
+   }
+
+   infix fun eller(other: Spesifikasjon<T>): Spesifikasjon<T> {
+      return Spesifikasjon(
+         beskrivelse = "$beskrivelse ELLER ${other.beskrivelse}",
+         identitet = "$identitet ELLER ${other.identitet}",
+         children = listOf(this, other),
+         implementasjon = { t -> evaluer(t) eller other.evaluer(t) }
+      )
+   }
+
+   fun ikke(): Spesifikasjon<T> {
+      return Spesifikasjon(
+         beskrivelse = "IKKE $beskrivelse",
+         identitet = "IKKE $identitet",
+         children = listOf(this),
+         implementasjon = { t -> evaluer(t).ikke() }
+      )
+   }
+
+   fun med(identitet: String, beskrivelse: String): Spesifikasjon<T> {
+      return this.copy(beskrivelse = beskrivelse, identitet = identitet)
+
+   }
+}
+
+fun <T> ikke(spec: Spesifikasjon<T>) = spec.ikke()
