51d0399a3e8eec1a34cd2dab907b6caec2de8a68
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-02-07 16:14:52 +0100

    Round the percentage up when displaying it

--- src/main/kotlin/no/nav/helse/sykepenger/vilkar/beregningsvilkar/Beregningsvilkår.kt
+++ src/main/kotlin/no/nav/helse/sykepenger/vilkar/beregningsvilkar/Beregningsvilkår.kt
@@ -22,7 +22,7 @@ fun behovForSkjønnsmessigVurderingAvInntekt(harVurdertInntekt: Boolean, aktuell
    } else {
       val avvik = Math.abs((12 * aktuellMånedsinntekt - rapportertInntekt) / rapportertInntekt.toDouble())
       if (avvik > 0.25) {
-         ja("Det er behov for skjønnsmessig vurdering av inntekt, avvik er ${avvik * 100} %")
+         ja("Det er behov for skjønnsmessig vurdering av inntekt, avvik er ${Math.ceil(avvik * 100)} %")
       } else {
          nei("Saksbehandler trenger ikke å vurdere inntekten")
       }
