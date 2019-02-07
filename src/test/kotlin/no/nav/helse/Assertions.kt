40e86694d30ddbde77790e990b54fd02cd3f63e3
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-02-07 15:55:00 +0100

    Fixed expected and actual argument order

--- src/test/kotlin/no/nav/helse/Assertions.kt
+++ src/test/kotlin/no/nav/helse/Assertions.kt
@@ -4,6 +4,6 @@ import no.nav.nare.core.evaluations.Evaluering
 import no.nav.nare.core.evaluations.Resultat
 import org.junit.jupiter.api.Assertions.assertEquals
 
-fun assertJa(evaluering: Evaluering) = assertEquals(evaluering.resultat, Resultat.JA)
-fun assertNei(evaluering: Evaluering) = assertEquals(evaluering.resultat, Resultat.NEI)
-fun assertKanskje(evaluering: Evaluering) = assertEquals(evaluering.resultat, Resultat.KANSKJE)
+fun assertJa(evaluering: Evaluering) = assertEquals(Resultat.JA, evaluering.resultat, evaluering.begrunnelse)
+fun assertNei(evaluering: Evaluering) = assertEquals(Resultat.NEI, evaluering.resultat, evaluering.begrunnelse)
+fun assertKanskje(evaluering: Evaluering) = assertEquals(Resultat.KANSKJE, evaluering.resultat, evaluering.begrunnelse)
